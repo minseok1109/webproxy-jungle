4d11877bd7fab6a506a1bcb2ac28ff8daec4ed03
minseok1109 <park485201@naver.com>
2023-04-19 14:08:10 +0000

    feat:finish cache

--- proxy.c
+++ proxy.c
@@ -1,224 +1,359 @@
 #include <stdio.h>
 #include "csapp.h"
 
-/* Recommended max cache and object sizes */
+#define WEBSERVER_HOST "localhost"
+#define WEBSERVER_PORT 8080
+
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
+#define CACHE_SIZE 10
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
-// static variables
-static const char *connection = "Connection: close\r\n";
-static const char *proxy_connection = "Proxy-Connection: close\r\n";
+static const char *request_line_hdr_format = "GET %s HTTP/1.0\r\n";
+static const char *endof_hdr = "\r\n";
+static const char *host_hdr_format = "Host: %s\r\n";
+static const char *conn_hdr = "Connection: close\r\n";
+static const char *prox_hdr = "Proxy-Connection: close\r\n";
 
-// functions
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
-void proxy(int connfd);
+static const char *host_header = "Host";
+static const char *connection_header = "Connection";
+static const char *proxy_connection_header = "Proxy-Connection";
+static const char *user_agent_header = "User-Agent";
+
+void doit(int connfd);
 void parse_uri(char *uri, char *hostname, char *path, int *port);
-void make_header(char *final_header, char *hostname, char *path, rio_t *client_rio);
+void build_http_header(char *http_header, char *hostname, char *path, rio_t *client_rio);
+int connect_webserver(char *hostname, int port);
+void *thread_function(void *arg);
+
+/* caching function */
+void cache_init();
+int cache_find(char *uri);
+void cache_uri(char *uri, char *response_buf);
+void get_cache_lock(int index);
+void put_cache_lock(int index);
+
+typedef struct
+{
+  char cache_obj[MAX_OBJECT_SIZE];
+  char cache_uri[MAXLINE];
+  int eviction_priority; // LRU 알고리즘에 의한 소거 우선순위. 숫자가 작을수록 소거에 대한 우선 순위가 높아짐
+  int is_empty;          // 이 블럭에 캐시 정보가 들었는지 empty인지 아닌지 체크
+
+  int reader_count; // reader 수
+  sem_t wmutex;     // cache block 쓰기 lock 여부
+  sem_t rdcntmutex; // cache block 읽기 lock 여부
+} cache_block;
+
+typedef struct
+{
+  cache_block cache_blocks[CACHE_SIZE];
+} Cache;
+
+Cache cache;
 
 int main(int argc, char **argv)
 {
-  /*
-  1. connfd의 내용을 읽어서
-  2. 알맞은 요청인지 확인 후 (Client_error)
-  3. 내용을 읽은 다음에
-  4. 새로운 fd를 만들어서 연결 시킨 후에 내용을 써서
-  5. server에서 요청을 읽고 Response 전달
-  */
-
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
+  int listenfd, *connfd_ptr;
   socklen_t clientlen;
+  char hostname[MAXLINE], port[MAXLINE];
+  pthread_t tid;
   struct sockaddr_storage clientaddr;
 
-  /* Check command line args */
+  cache_init();
+
   if (argc != 2)
   {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    fprintf(stderr, "usage: %s <port> \n", argv[0]);
     exit(1);
   }
+  // 프로세스가 닫히거나 끊어진 파이프에 쓰기 요청을 할 경우 발생하는 오류(SIGPIPE)를 무시하고 서버를 계속 동작시킬 수 있음
+  Signal(SIGPIPE, SIG_IGN);
 
   listenfd = Open_listenfd(argv[1]);
+
+  int connfd;
+
   while (1)
   {
     clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr,
-                    &clientlen);
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
-    printf("Accepted connection from (%s, %s)\n", hostname, port);
-    proxy(connfd);
-    Close(connfd);
+    connfd_ptr = (int *)malloc(sizeof(int));
+    *connfd_ptr = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    printf("Accepted connection from (%s %s).\n", hostname, port);
+
+    Pthread_create(&tid, NULL, thread_function, connfd_ptr);
   }
   return 0;
 }
 
-void proxy(int connfd)
+void *thread_function(void *arg)
 {
-  int server_connected_fd;
+  Pthread_detach(Pthread_self());
+
+  int connfd = *((int *)arg);
+  free(arg);
+  doit(connfd);
+  Close(connfd);
+}
+
+void doit(int connfd)
+{
+  int web_connfd, port;
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-  char hostname[MAXLINE], path[MAXLINE], to_server_header[MAXLINE];
-  int port;
-  rio_t client_rio, server_rio;
+  char webserver_http_header[MAXLINE];
+  char hostname[MAXLINE], path[MAXLINE];
 
-  Rio_readinitb(&client_rio, connfd);
-  Rio_readlineb(&client_rio, buf, MAXLINE);
+  rio_t rio, server_rio;
 
-  printf("proxy headers:\n");
+  Rio_readinitb(&rio, connfd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request headers: \n");
   printf("%s", buf);
-  sscanf(buf, "%s %s %s", method, uri, version);
+  sscanf(buf, "%s %s %s", method, uri, version); // read the client reqeust line
 
-  // GET말고 다른 요청 보내면 return
   if (strcasecmp(method, "GET"))
   {
-    clienterror(connfd, method, "501", "Not implemented", "Proxy does not implemet this method");
+    printf("Proxy does not implement the method");
     return;
   }
 
-  parse_uri(uri, hostname, path, &port);
-  printf("proxy server hostname: %s\r\n", hostname);
-  printf("proxy server port: %d\r\n", port);
-  printf("proxy server path: %s\r\n", path);
+  char uri_copy[100];
+  strcpy(uri_copy, uri);
 
-  // Tiny 서버와 연결
-  make_header(to_server_header, hostname, path, &client_rio);
-  server_connected_fd = connect_server(hostname, port);
+  /* 요청 uri 주소가 캐싱되어 있는 주소 인지 */
+  int cache_index;
+  if ((cache_index = cache_find(uri_copy)) != -1)
+  {
+    get_cache_lock(cache_index); // 캐시 뮤텍스를 풀어줌 (열어줌 0->1)
+    // 캐시에서 찾은 값을 connfd에 쓰고, 캐시에서 그 값을 바로 보내게 됨
+    Rio_writen(connfd, cache.cache_blocks[cache_index].cache_obj, strlen(cache.cache_blocks[cache_index].cache_obj));
+    put_cache_lock(cache_index); // 닫아줌 1->0 doit 끝
+    return;
+  }
 
-  // 넘겨줄 헤더 작성
+  parse_uri(uri, hostname, path, &port);                          // uri 로부터 hostname, path, port 파싱하여 변수에 할당
+  build_http_header(webserver_http_header, hostname, path, &rio); // hostname, path, port와 클라이언트 요청을 기반으로 웹 서버에 전송할 요청 헤더 재구성
 
-  // printf("%s\n", to_server_header);
+  web_connfd = connect_webserver(hostname, port); // 소켓 생성, 웹 서버와 연결
+  if (web_connfd < 0)
+  {
+    printf("connection failed\n");
+    return;
+  }
 
-  // send to tiny server
-  Rio_readinitb(&server_rio, server_connected_fd);
-  Rio_writen(server_connected_fd, to_server_header, strlen(to_server_header));
-  printf("서버 헤더: %s\n", to_server_header);
-  // 서버로부터 응답을 받아 클라이언트에 전송
+  Rio_readinitb(&server_rio, web_connfd);
+  Rio_writen(web_connfd, webserver_http_header, strlen(webserver_http_header)); // 웹 서버로 재구성한 요청 헤더를 전송
+
+  char response_buf[MAX_OBJECT_SIZE];
+  int size_buf = 0;
   size_t n;
+
+  /* 웹 서버 응답을 한 줄씩 읽어서 클라이언트에게 전달 */
   while ((n = Rio_readlineb(&server_rio, buf, MAXLINE)) != 0)
   {
-    printf("proxy received %d bytes, then send them to client\n", n);
-    // 서버의 응답을 클라이언트에게 forward
-    Rio_writen(connfd, buf, n); // client와의 연결 소켓인 connfd에 쓴다
+    size_buf += n;
+    /* proxy거쳐서 서버에서 response오는데, 그 응답을 저장하고 클라이언트에 보냄 */
+    if (size_buf < MAX_OBJECT_SIZE) // response_buf에 제한 두지 않고 계속 쓰다보면 buffer overflow 발생
+      strcat(response_buf, buf);
+    Rio_writen(connfd, buf, n);
   }
-  Close(server_connected_fd);
+
+  Close(web_connfd);
+
+  /* 저장된 response_buf의 크기가 cache block에 저장될 수 있는 최대 크기보다 작을때만 캐싱 */
+  if (size_buf < MAX_OBJECT_SIZE)
+    cache_uri(uri_copy, response_buf);
 }
 
-// other도 추가
-void make_header(char *final_header, char *hostname, char *path, rio_t *client_rio)
+void build_http_header(char *http_header, char *hostname, char *path,
+                       rio_t *client_rio)
 {
-  char request_header[MAXLINE], buf[MAXLINE], host_header[MAXLINE], other[MAXLINE];
+  char buf[MAXLINE], request_line[MAXLINE], other_hdr[MAXLINE],
+      host_hdr[MAXLINE];
+  /* request line 생성 */
+  sprintf(request_line, request_line_hdr_format, path);
 
-  sprintf(request_header, "GET %s HTTP/1.0\r\n", path);
-  sprintf(host_header, "Host: %s\r\n", hostname); // 호스트헤더는 기 요청받은 호스트네임으로
-
-  /* 클라이언트의 나머지 요청 받아서 저장 */
-  while (Rio_readlineb(client_rio, buf, MAXLINE))
+  /* 클라이언트 입력 스트림 버퍼를 한 줄씩 읽어서 HTTP header를 만듦 */
+  while (Rio_readlineb(client_rio, buf, MAXLINE) > 0)
   {
-    if (!(strcmp("\r\n", buf)))
+    if (strcmp(buf, endof_hdr) == 0)
+      break;
+
+    /* 대소문자 여부 상관 없이 비교 if true -> return 0 */
+    if (!strncasecmp(buf, host_header, strlen(host_header)))
     {
-      break; // '\r\n' 이면 끝
+      strcpy(host_hdr, buf);
+      continue;
     }
 
-    /* 얘네는 정해진 형식대로 채워줄거임 */
-    if (strncasecmp(buf, "User-Agent", strlen("User-Agent")) && strncasecmp(buf, "Connection", strlen("Connection")) && strncasecmp(buf, "Proxy-Connection", strlen("Proxy-Connection")))
+    /* 기타 헤더 정보 */
+    if (!strncasecmp(buf, connection_header, strlen(connection_header)) &&
+        !strncasecmp(buf, proxy_connection_header, strlen(proxy_connection_header)) &&
+        !strncasecmp(buf, user_agent_header, strlen(user_agent_header)))
     {
-      strcat(other, buf); // 따라서 정해진 형식없는 애들만 other에 넣어줌(이어붙이기)
+      strcat(other_hdr, buf);
     }
   }
+  if (strlen(host_hdr) == 0)
+    sprintf(host_hdr, host_hdr_format, hostname);
+  sprintf(http_header, "%s%s%s%s%s%s%s", request_line, host_hdr, conn_hdr,
+          prox_hdr, user_agent_hdr, other_hdr, endof_hdr);
+  printf("%s\n", http_header);
 
-  /* 최종 요청 헤더의 모습 */
-  sprintf(final_header, "%s%s%s%s%s%s%s",
-          request_header,
-          host_header,
-          user_agent_hdr,
-          connection,
-          proxy_connection,
-          other,
-          "\r\n");
+  return;
 }
-
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg)
+int connect_webserver(char *hostname, int port)
 {
-  char buf[MAXLINE], body[MAXLINE];
-
-  sprintf(body, "<html><title>Tiny Error</title>");
-  sprintf(body, "%s<body bgcolor="
-                "ffffff"
-                ">\r\n",
-          body);
-  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
-  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-  sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
-
-  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
-  Rio_writen(fd, buf, strlen(buf));
-  sprintf(buf, "Content-type: text/html\r\n");
-  Rio_writen(fd, buf, strlen(buf));
-  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
-  Rio_writen(fd, buf, strlen(buf));
-  Rio_writen(fd, body, strlen(body));
+  char port_str[100];
+  sprintf(port_str, "%d", port);
+  return Open_clientfd(hostname, port_str);
 }
 
+/* 요청된 uri로부터 hostname, path, port를 parsing */
 void parse_uri(char *uri, char *hostname, char *path, int *port)
 {
-  *port = 80;  // 별 언급 없을시 default
-  *path = '/'; // 기본 path
-  char *hPos;
-  char *pPos;
+  /* default webserver host, port */
+  strcpy(hostname, WEBSERVER_HOST);
+  *port = WEBSERVER_PORT;
 
-  /* host 부분부터 parse */
-  /* http:// 가 달린 요청인경우 */
-  // http://localhost:3000
-  hPos = strstr(uri, "//");
-  if (hPos != NULL)
-  {
-    hPos = hPos + 2; /*  // 이거 두개 건너뛰어야함 */
-  }
-  else
-  {
-    hPos = uri;
-  }
+  /* http:// 이후의 host:port/path parsing */
+  char *pos = strstr(uri, "//");
+  pos = pos != NULL ? pos + 2 : uri;
 
-  /* port와 path를 parse */
-  pPos = strstr(hPos, ":");
-  // 여기서 strstr의 첫번째 인자를 uri로 두면, http://~~ 와 같은 요청이 들어왔을때
-  // pPos를 http바로 다음 : 위치로 잡게됨
-  if (pPos != NULL)
-  { // 따로 지정된 포트가 있음
-    *pPos = '\0';
-    sscanf(hPos, "%s", hostname);
-    sscanf(pPos + 1, "%d%s", port, path); // 여기서 segfault
-  }
-  else // 따로 지정된 포트가 없음
+  /* host: 이후의 port/path parsing*/
+  char *pos2 = strstr(pos, ":");
+
+  /* port 번호를 포함하여 요청했다면  */
+  if (pos2 != NULL)
   {
-    pPos = strstr(hPos, "/");
-    if (!pPos)
-    { // 뒤의 path가 아예없음
-      sscanf(hPos, "%s", hostname);
-    }
-    else
-    { // 뒤의 path가 있음
-      *pPos = '\0';
-      sscanf(hPos, "%s", hostname);
-      *pPos = '/'; // path부분 긁어야 하므로 다시 / 로 바꿔줌
-      sscanf(pPos, "%s", path);
+    *pos2 = '\0';
+    sscanf(pos2 + 1, "%d%s", port, path); // 숫자는 port에 이후 문자열은 path에 저장
+  }
+  else /* port 번호가 없이 요청 왔다면 */
+  {
+    pos2 = strstr(pos, "/");
+    if (pos2 != NULL) // path를 통해 특정 자원에 대한 요청이 있을 경우
+    {
+      sscanf(pos2, "%s", path); // pos2 위치의 문자열을 path에 저장함
     }
   }
   return;
 }
 
-int connect_server(char *hostname, int port)
-
+/* 캐시 초기화 함수 */
+void cache_init()
 {
-  int clientfd;
-  char port_s[MAXLINE];
-  sprintf(port_s, "%d", port);
-  clientfd = open_clientfd(hostname, port_s);
+  for (int i = 0; i < CACHE_SIZE; i++)
+  {
+    cache.cache_blocks[i].eviction_priority = 0; // 아직 캐싱된 데이터 없으므로 0, 최근에 할당 된 cache block 일 수록 높은 값을 가짐
+    cache.cache_blocks[i].is_empty = 1;          // 아직 캐싱된 데이터 없으므로 1
 
-  return clientfd;
+    // 두번째 파라미터 1이면 process shared, 0이면 thread shared, 세번째 파라미터 -> 세마포어 초기값 1(액세스 가능)
+    Sem_init(&cache.cache_blocks[i].wmutex, 0, 1);     // -> 진입 가능한 자원 1개뿐이므로 binary semaphore
+    Sem_init(&cache.cache_blocks[i].rdcntmutex, 0, 1); // -> 진입 가능한 자원 1개뿐이므로 binary semaphore
+    cache.cache_blocks[i].reader_count = 0;
+  }
+}
+
+/* cache block 접근 전 cache block access lock을 얻기 위한 함수 */
+void get_cache_lock(int index)
+{
+  P(&cache.cache_blocks[index].rdcntmutex); // reader count 값 변경에 대한 lock 획득
+
+  cache.cache_blocks[index].reader_count++;        // read count 증가(조회 하러 들어가므로)
+  if (cache.cache_blocks[index].reader_count == 1) // reader_count == 1 -> 현재 읽는 사용자 한명만 캐시 블록에 접근 중
+    P(&cache.cache_blocks[index].wmutex);          // cache block에 대한 write lock 획득
+
+  V(&cache.cache_blocks[index].rdcntmutex); // reader count 값 변경에 대한 lock 반환
+}
+
+/* cache block 접근 끝난 이후 cache block access lock을 반환하기 위한 함수 */
+void put_cache_lock(int index)
+{
+  P(&cache.cache_blocks[index].rdcntmutex); // reader count 값 변경에 대한 lock 획득
+
+  cache.cache_blocks[index].reader_count--;        // read count 감소(조회 끝났으므로)
+  if (cache.cache_blocks[index].reader_count == 0) // reader_count == 0 -> 현재 읽는 사용자 한명만 캐시 블록에 접근 중
+    V(&cache.cache_blocks[index].wmutex);          // cache block에 대한 write lock 획득
+
+  V(&cache.cache_blocks[index].rdcntmutex); // reader count 값 변경에 대한 lock 획득
+}
+
+/* cache 에서 요청 uri와 일치하는 uri를 가지고 있는 cache block을 탐색하여 해당 block의 index 반환 */
+int cache_find(char *uri)
+{
+  printf("\ncache hit ! ====> %s\n", uri);
+  for (int i = 0; i < CACHE_SIZE; i++)
+  {
+    get_cache_lock(i);
+    /* cache block 이 empty 가 아니고, cache block에 있는 uri이 현재 요청 uri과 일치한다면 cache block의 index 반환 */
+    if (strcmp(uri, cache.cache_blocks[i].cache_uri) == 0)
+    {
+      put_cache_lock(i);
+      return i;
+    }
+    put_cache_lock(i);
+  }
+  return -1;
+}
+
+/* eviction_priority 알고리즘에 따라 최소 eviction_priority 값을 갖는 cache block의 index 찾아 반환 */
+int cache_eviction()
+{
+  int min = CACHE_SIZE;
+  int minindex = 0;
+  for (int i = 0; i < CACHE_SIZE; i++)
+  {
+    get_cache_lock(i);
+    /* cache block empty 라면 해당 block의 index를 반환 */
+    if (cache.cache_blocks[i].is_empty == 1)
+    {
+      put_cache_lock(i);
+      return i;
+    }
+    /* eviction_priority가 현재 최솟값 min 보다 작다면 eviction_priority 값을 갱신 해주면서 최소 cache block 탐색*/
+    if (cache.cache_blocks[i].eviction_priority < min)
+    {
+      minindex = i;                                  // i로 minindex 갱신
+      min = cache.cache_blocks[i].eviction_priority; // min은 i번째 cache block의 eviction_priority 값으로 갱신
+    }
+    put_cache_lock(i);
+  }
+  return minindex;
+}
+
+void cache_eviction_priority(int index)
+{
+  for (int i = 0; i < CACHE_SIZE; i++)
+  {
+    if (i == index)
+      continue;
+
+    P(&cache.cache_blocks[i].wmutex); // cache block 쓰기 lock 획득
+
+    if (cache.cache_blocks[i].is_empty == 0)
+      cache.cache_blocks[i].eviction_priority--; // 최근 캐싱된 cache block을 제외한 나머지 cache block eviction_priority 값을 감소 시킴
+
+    V(&cache.cache_blocks[i].wmutex); // cache block 쓰기 lock 반환
+  }
+}
+
+/* empty cache block에 uri 캐싱 */
+void cache_uri(char *uri, char *response_buf)
+{
+  int index = cache_eviction(); // 빈 캐시 블럭을 찾는 첫번째 index
+
+  P(&cache.cache_blocks[index].wmutex); // cache block 쓰기 lock 획득
+
+  strcpy(cache.cache_blocks[index].cache_obj, response_buf); // 웹 서버 응답 값을 캐시 블록에 저장
+  strcpy(cache.cache_blocks[index].cache_uri, uri);          // 클라이언트의 요청 uri를 캐시 블록에 저장
+  cache.cache_blocks[index].is_empty = 0;                    // 캐시 블록 할당 되었으므로 0으로 변경
+  cache.cache_blocks[index].eviction_priority = CACHE_SIZE;  // 가장 최근 캐싱 되었으므로, 가장 큰 값 부여
+  cache_eviction_priority(index);                            // 기존 나머지 캐시 블록들의 eviction_priority 값을 낮추어서 eviction 우선 순위를 높임
+
+  V(&cache.cache_blocks[index].wmutex); // cache block 쓰기 lock 반환
 }
